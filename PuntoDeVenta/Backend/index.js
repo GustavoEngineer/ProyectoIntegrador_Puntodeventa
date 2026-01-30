fb6361c69d42c296ed35338b537412eca21c87dc
Gustavo Marrufo <gustavitotlgac@gmail.com>
2026-01-30 13:34:43 -0600

    Deleted files

--- PuntoDeVenta/Backend/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./src/app');
-require('dotenv').config();
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
